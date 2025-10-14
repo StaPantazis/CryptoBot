1fa7c9d7c1ba4d321a9519c951ff32765ae1753f
StaPantazis <stathispantazis7@gmail.com>
2025-10-14 23:12:35 +0300

    Minor changes after call

---
+++ Cryptobot.ConsoleApp/Backtesting/Strategies/TradeStrategies/TS_Simple_120_days_SMA_Mix.cs
@@ -0,0 +1,66 @@
+using Cryptobot.ConsoleApp.EngineDir.Models;
+using Cryptobot.ConsoleApp.EngineDir.Models.Enums;
+using Cryptobot.ConsoleApp.Services;
+using Cryptobot.ConsoleApp.Utils;
+
+namespace Cryptobot.ConsoleApp.Backtesting.Strategies.TradeStrategies;
+
+public class TS_Simple_120_days_SMA_Mix : TradeStrategyBase
+{
+    public override string Name { get; protected set; } = "SMA Min_Long_Short";
+    public override string NameOf { get; protected set; } = nameof(TS_Simple_120_days_SMA_short);
+    public override IndicatorType[] RelevantIndicators { get; protected set; } = [IndicatorType.MovingAverage, IndicatorType.MacroTrend];
+
+    protected override double? StopLossLong<T>(List<T> candles, int currentCandleIndex) => null;
+    protected override double? TakeProfitLong<T>(List<T> candles, int currentCandleIndex) => null;
+
+    protected override double? StopLossShort<T>(List<T> candles, int currentCandleIndex) => null;
+    protected override double? TakeProfitShort<T>(List<T> candles, int currentCandleIndex) => null;
+
+    public override bool ShouldExitLongTrade<T>(CacheService cacheManager, List<T> candles, int currentCandleIndex, Trade trade)
+    {
+        if (currentCandleIndex < Constants.FIFTEEN_MINUTE_CANDLES_120_DAYS)
+        {
+            return false;
+        }
+
+        var currentCandle = candles[currentCandleIndex];
+        return currentCandle.ClosePrice < cacheManager.MacroTrendCache[currentCandle.DayTicks].MA120d;
+    }
+
+    public override bool ShouldExitShortTrade<T>(CacheService cacheManager, List<T> candles, int currentCandleIndex, Trade trade)
+    {
+        if (currentCandleIndex < Constants.FIFTEEN_MINUTE_CANDLES_120_DAYS)
+        {
+            return false;
+        }
+
+        var currentCandle = candles[currentCandleIndex];
+        return currentCandle.ClosePrice > cacheManager.MacroTrendCache[currentCandle.DayTicks].MA120d;
+    }
+
+    protected override bool ShouldLong<T>(CacheService cacheManager, List<T> candles, int currentCandleIndex)
+    {
+        if (currentCandleIndex < Constants.FIFTEEN_MINUTE_CANDLES_120_DAYS)
+        {
+            return false;
+        }
+
+        var previousCandle = candles[currentCandleIndex - 1];
+        var candle = candles[currentCandleIndex];
+        return candle.ClosePrice > cacheManager.MacroTrendCache[candle.DayTicks].MA120d
+            && previousCandle.ClosePrice < cacheManager.MacroTrendCache[candle.DayTicks].MA120d;
+    }
+
+    protected override bool ShouldShort<T>(CacheService cacheManager, List<T> candles, int currentCandleIndex)
+    {
+        return false;
+        if (currentCandleIndex < Constants.FIFTEEN_MINUTE_CANDLES_120_DAYS)
+        {
+            return false;
+        }
+
+        var currentCandle = candles[currentCandleIndex];
+        return currentCandle.ClosePrice < cacheManager.MacroTrendCache[currentCandle.DayTicks].MA120d;
+    }
+}
--- Cryptobot.ConsoleApp/Consoler.cs
+++ Cryptobot.ConsoleApp/Consoler.cs
@@ -18,9 +18,11 @@ public static class Consoler
             Symbol: Constants.SYMBOL_BTCUSDT,
             MarketCategory: Constants.MARKET_PERPETUAL_FUTURES,
             Strategies: [
-                new StrategyBundle<TS_Every100Candles_SL5_TP5, BS_1Percent>(),
+                //new StrategyBundle<TS_Every100Candles_SL5_TP5, BS_1Percent>(),
                 //new StrategyBundle<TS_LongWhenHigherThan50MAAndNeutralOrBullish_SL3_TP3, BS_100Percent>(),
-                //new StrategyBundle<TS_Simple_120_days_SMA, BS_1Percent>(),
+                new StrategyBundle<TS_Simple_120_days_SMA_Mix, BS_100Percent>(),
+                //new StrategyBundle<TS_Simple_120_days_SMA_short, BS_1Percent>(),
+                //new StrategyBundle<TS_Simple_120_days_SMA_Mix, BS_1Percent>(),
                 //new StrategyBundle<VariationSandboxFactory, BS_100Percent>(),
                 ]);
 
